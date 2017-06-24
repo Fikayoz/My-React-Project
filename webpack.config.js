60c1f85f6cfc32c7b3089c1c12ff1317e7f597c5
Fikayoz <Fikayoz@users.noreply.github.com>
2017-06-24 19:54:12 -0400

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
-var HTMLWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-	template: __dirname + '/Base.html',
-	filename: '/new_base.html',
-	inject: 'body'
-});
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-	entry: __dirname + '/To-Do/code.js',
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: 'app.bundle.js'
+	},
 	module: {
-		loaders: [
+		rules: [
 			{
-				test:/\.js$/,
+				test: /\.scss$/, 
+				use: ExtractTextPlugin.extract({
+					fallbackLoader: 'style-loader',
+					loader: ['css-loader','sass-loader'],
+					publicPath: '/dist'
+				})
+			},
+			{
+				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
+				use: 'babel-loader'
 			}
 		]
 	},
-	output: {
-		filename: 'transformed.js',
-		path: __dirname + '/build'
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		stats: 'errors-only',
+		open: true
 	},
-	plugins: [HTMLWebpackPluginConfig]
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Project Demo',
+			minify: {
+				collapseWhitespace: false
+			},
+			hash: true,
+			template: './src/index.html',
+		}),
+		new ExtractTextPlugin({
+			filename: 'app.css',
+			disabled: false,
+			allChunks: true
+		})
+	]
 };
