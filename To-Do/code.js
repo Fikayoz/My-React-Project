50810c28eeafebcd6fab2fa1d182cefa40bfcc76
Fikayoz <Fikayoz@users.noreply.github.com>
2017-06-25 16:59:20 -0400

    Delete code.js

--- To-Do/code.js
+++
@@ -1,7 +0,0 @@
-// Just some base placeholder code
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var test = <h1>This is a test</h1>;
-
-ReactDOM.render(test, document.getElementById('app'));
